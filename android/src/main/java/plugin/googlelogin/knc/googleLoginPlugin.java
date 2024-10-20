c247048e716532a92987e34b6bd36aba93e1c0af
hyseo <hyseo@kncuration.com>
2024-10-20 12:20:57 +0900

    [Fix] 투니고로 재확인필요

--- android/src/main/java/plugin/googlelogin/knc/googleLoginPlugin.java
+++ android/src/main/java/plugin/googlelogin/knc/googleLoginPlugin.java
@@ -1,86 +1,39 @@
 package plugin.googlelogin.knc;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.accounts.AccountManagerFuture;
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
-import androidx.activity.result.ActivityResult;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.ActivityCallback;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
 
 @CapacitorPlugin(name = "googleLogin")
 public class googleLoginPlugin extends Plugin {
 
-    // see https://developers.google.com/android/reference/com/google/android/gms/auth/api/signin/GoogleSignInStatusCodes#SIGN_IN_CANCELLED
-    private static final int SIGN_IN_CANCELLED = 12501;
-
-    private GoogleSignInClient googleSignInClient;
-
-    public void loadSignInClient(String clientId) {
-        Log.d("googleLogin", "loadSignInClient: " + clientId);
-        GoogleSignInOptions.Builder googleSignInBuilder = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(clientId)
-            .requestEmail();
-
-        GoogleSignInOptions googleSignInOptions = googleSignInBuilder.build();
-        googleSignInClient = GoogleSignIn.getClient(this.getContext(), googleSignInOptions);
-    }
-
-    @Override
-    public void load() {}
+    private static PluginCall googleCall;
 
     @PluginMethod
     public void googleLogin(PluginCall call) {
-        String googleClientId = call.getString("googleClientId");
+        Log.d("googleLogin", "googleLogin: 1");
+        googleCall = call;
 
-        Log.d("googleLogin", "googleLogin: " + googleClientId);
-        loadSignInClient(googleClientId);
-
-        if (googleSignInClient == null) {
-            rejectWithNullClientError(call);
-            return;
-        }
-
-        Intent signInIntent = googleSignInClient.getSignInIntent();
-        startActivityForResult(call, signInIntent, "signInResult");
+        Intent intent = new Intent(getActivity(), googleLogin.class);
+        getActivity().startActivity(intent);
     }
 
-    @ActivityCallback
-    protected void signInResult(PluginCall call, ActivityResult result) {
-        if (call == null) return;
-
-        Task<GoogleSignInAccount> completedTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
-
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            String email = account.getEmail();
-
+    public void returnGoogleLogin(String email) {
+        Log.d("googleLogin", "googleLogin: 4");
+        if (email != null) {
             JSObject ret = new JSObject();
             ret.put("email", email);
-            call.resolve(ret);
-        } catch (ApiException e) {
-            if (SIGN_IN_CANCELLED == e.getStatusCode()) {
-                call.reject("The user canceled the sign-in flow.", "" + e.getStatusCode());
-            } else {
-                call.reject("Something went wrong", "" + e.getStatusCode());
-            }
+            googleCall.resolve(ret);
+        } else {
+            googleCall.reject("하영님에게 문의하세요");
         }
-    }
-
-    private void rejectWithNullClientError(final PluginCall call) {
-        call.reject("Google services are not ready. Please call initialize() first");
     }
 }
